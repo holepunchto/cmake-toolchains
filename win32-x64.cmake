157859d40cddd485e3a3b83fb679d29fc14af510
Kasper Isager Dalsgarð <kasperisager@hey.com>
2024-10-16 15:32:04 +0200

    Use Clang directly on Windows

--- win32-x64.cmake
+++ win32-x64.cmake
@@ -1,4 +1,11 @@
 set(CMAKE_SYSTEM_NAME Windows)
+set(CMAKE_SYSTEM_PROCESSOR x64)
 
-set(CMAKE_GENERATOR_TOOLSET ClangCL)
-set(CMAKE_GENERATOR_PLATFORM x64)
+set(target x86_64-pc-win32)
+
+set(CMAKE_C_COMPILER clang.exe)
+set(CMAKE_C_COMPILER_TARGET ${target})
+set(CMAKE_CXX_COMPILER clang++.exe)
+set(CMAKE_CXX_COMPILER_TARGET ${target})
+set(CMAKE_ASM_COMPILER clang.exe)
+set(CMAKE_ASM_COMPILER_TARGET ${target})
