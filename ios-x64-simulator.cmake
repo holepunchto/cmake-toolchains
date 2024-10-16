7396151c012f5c59a8373dd66072cce9b60d4459
Kasper Isager Dalsgarð <kasperisager@hey.com>
2024-10-16 15:16:17 +0200

    Add ASM support

--- ios-x64-simulator.cmake
+++ ios-x64-simulator.cmake
@@ -7,6 +7,8 @@ set(CMAKE_C_COMPILER clang)
 set(CMAKE_C_COMPILER_TARGET ${target})
 set(CMAKE_CXX_COMPILER clang++)
 set(CMAKE_CXX_COMPILER_TARGET ${target})
+set(CMAKE_ASM_COMPILER clang)
+set(CMAKE_ASM_COMPILER_TARGET ${target})
 
 set(CMAKE_OSX_SYSROOT iphonesimulator)
 set(CMAKE_OSX_DEPLOYMENT_TARGET 14.0)
