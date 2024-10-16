c47d58457f30edc3e562face4c09ad6e2c07dace
Kasper Isager Dalsgarð <kasperisager@hey.com>
2024-10-16 14:31:54 +0200

    Move around `OSX_*` attributes

--- ios-arm64.cmake
+++ ios-arm64.cmake
@@ -1,8 +1,6 @@
 set(CMAKE_SYSTEM_NAME Darwin)
 set(CMAKE_SYSTEM_PROCESSOR arm64)
 
-set(CMAKE_OSX_SYSROOT iphoneos)
-
 set(target arm64-apple-ios14.0)
 
 set(CMAKE_C_COMPILER clang)
@@ -10,4 +8,5 @@ set(CMAKE_C_COMPILER_TARGET ${target})
 set(CMAKE_CXX_COMPILER clang++)
 set(CMAKE_CXX_COMPILER_TARGET ${target})
 
+set(CMAKE_OSX_SYSROOT iphoneos)
 set(CMAKE_OSX_DEPLOYMENT_TARGET 14.0)
