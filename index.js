33f890ccfd958b28cbfe1537528a23591eae01cc
Kasper Isager Dalsgarð <kasperisager@hey.com>
2025-01-07 13:21:57 +0100

    Use new asset syntax

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
-const asset = require('require-asset')
+require.asset = require('require-asset')
 
-exports['android-arm'] = asset('./android-arm.cmake', __filename)
-exports['android-arm64'] = asset('./android-arm64.cmake', __filename)
-exports['android-ia32'] = asset('./android-ia32.cmake', __filename)
-exports['android-x64'] = asset('./android-x64.cmake', __filename)
-exports['darwin-arm64'] = asset('./darwin-arm64.cmake', __filename)
-exports['darwin-x64'] = asset('./darwin-x64.cmake', __filename)
-exports['ios-arm64'] = asset('./ios-arm64.cmake', __filename)
-exports['ios-arm64-simulator'] = asset(
+exports['android-arm'] = require.asset('./android-arm.cmake', __filename)
+exports['android-arm64'] = require.asset('./android-arm64.cmake', __filename)
+exports['android-ia32'] = require.asset('./android-ia32.cmake', __filename)
+exports['android-x64'] = require.asset('./android-x64.cmake', __filename)
+exports['darwin-arm64'] = require.asset('./darwin-arm64.cmake', __filename)
+exports['darwin-x64'] = require.asset('./darwin-x64.cmake', __filename)
+exports['ios-arm64'] = require.asset('./ios-arm64.cmake', __filename)
+exports['ios-arm64-simulator'] = require.asset(
   './ios-arm64-simulator.cmake',
   __filename
 )
-exports['ios-x64-simulator'] = asset('./ios-x64-simulator.cmake', __filename)
-exports['linux-arm64'] = asset('./linux-arm64.cmake', __filename)
-exports['linux-x64'] = asset('./linux-x64.cmake', __filename)
-exports['win32-arm64'] = asset('./win32-arm64.cmake', __filename)
-exports['win32-x64'] = asset('./win32-x64.cmake', __filename)
+exports['ios-x64-simulator'] = require.asset(
+  './ios-x64-simulator.cmake',
+  __filename
+)
+exports['linux-arm64'] = require.asset('./linux-arm64.cmake', __filename)
+exports['linux-x64'] = require.asset('./linux-x64.cmake', __filename)
+exports['win32-arm64'] = require.asset('./win32-arm64.cmake', __filename)
+exports['win32-x64'] = require.asset('./win32-x64.cmake', __filename)
