719a07e2d562777bbb4f94a1597957b8cbb0aae4
Parallels <kasperisager@hey.com>
2025-02-25 09:54:43 +0100

    Add MIPS musl toolchains

---
+++ linux-mips-muslsf.cmake
@@ -0,0 +1,29 @@
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_PROCESSOR mips)
+
+find_program(
+  cc
+  NAMES mips-linux-muslsf-cc
+  REQUIRED
+)
+
+find_program(
+  c++
+  NAMES mips-linux-muslsf-c++
+  REQUIRED
+)
+
+set(target mips-linux-muslsf)
+
+set(CMAKE_C_COMPILER ${cc})
+set(CMAKE_C_COMPILER_TARGET ${target})
+
+set(CMAKE_CXX_COMPILER ${c++})
+set(CMAKE_CXX_COMPILER_TARGET ${target})
+
+set(CMAKE_ASM_COMPILER ${cc})
+set(CMAKE_ASM_COMPILER_TARGET ${target})
+
+set(CMAKE_EXE_LINKER_FLAGS_INIT "-static")
+
+set(VCPKG_TARGET_TRIPLET mips-linux-muslsf)
---
+++ linux-mipsel-muslsf.cmake
@@ -0,0 +1,29 @@
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_PROCESSOR mipsel)
+
+find_program(
+  cc
+  NAMES mipsel-linux-muslsf-cc
+  REQUIRED
+)
+
+find_program(
+  c++
+  NAMES mipsel-linux-muslsf-c++
+  REQUIRED
+)
+
+set(target mipsel-linux-muslsf)
+
+set(CMAKE_C_COMPILER ${cc})
+set(CMAKE_C_COMPILER_TARGET ${target})
+
+set(CMAKE_CXX_COMPILER ${c++})
+set(CMAKE_CXX_COMPILER_TARGET ${target})
+
+set(CMAKE_ASM_COMPILER ${cc})
+set(CMAKE_ASM_COMPILER_TARGET ${target})
+
+set(CMAKE_EXE_LINKER_FLAGS_INIT "-static")
+
+set(VCPKG_TARGET_TRIPLET mipsel-linux-muslsf)
