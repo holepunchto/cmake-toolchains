314dba9781295033d564649aed912c799d4576ea
Kasper Isager Dalsgarð <kasperisager@hey.com>
2025-02-25 09:55:54 +0100

    Formatting

--- index.js
+++ index.js
@@ -17,11 +17,23 @@ exports['ios-x64-simulator'] = require.asset(
 )
 exports['linux-arm64'] = require.asset('./linux-arm64.cmake', __filename)
 exports['linux-mips'] = require.asset('./linux-mips.cmake', __filename)
-exports['linux-mips-musl'] = require.asset('./linux-mips-musl.cmake', __filename)
-exports['linux-mips-muslsf'] = require.asset('./linux-mips-muslsf.cmake', __filename)
+exports['linux-mips-musl'] = require.asset(
+  './linux-mips-musl.cmake',
+  __filename
+)
+exports['linux-mips-muslsf'] = require.asset(
+  './linux-mips-muslsf.cmake',
+  __filename
+)
 exports['linux-mipsel'] = require.asset('./linux-mipsel.cmake', __filename)
-exports['linux-mipsel-musl'] = require.asset('./linux-mipsel-musl.cmake', __filename)
-exports['linux-mipsel-muslsf'] = require.asset('./linux-mipsel-muslsf.cmake', __filename)
+exports['linux-mipsel-musl'] = require.asset(
+  './linux-mipsel-musl.cmake',
+  __filename
+)
+exports['linux-mipsel-muslsf'] = require.asset(
+  './linux-mipsel-muslsf.cmake',
+  __filename
+)
 exports['linux-x64'] = require.asset('./linux-x64.cmake', __filename)
 exports['win32-arm64'] = require.asset('./win32-arm64.cmake', __filename)
 exports['win32-x64'] = require.asset('./win32-x64.cmake', __filename)
