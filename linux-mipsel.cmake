163cae2aac0958f4d99194de460f9467c3f931ba
Kasper Isager Dalsgarð <kasperisager@hey.com>
2025-04-28 09:52:16 +0200

    Support LLVM 21 on Linux

--- linux-mipsel.cmake
+++ linux-mipsel.cmake
@@ -1,17 +1,7 @@
 set(CMAKE_SYSTEM_NAME Linux)
 set(CMAKE_SYSTEM_PROCESSOR mipsel)
 
-find_program(
-  clang
-  NAMES clang-20 clang-19 clang-18 clang
-  REQUIRED
-)
-
-find_program(
-  clang++
-  NAMES clang++-20 clang++-19 clang++-18 clang++
-  REQUIRED
-)
+include("${CMAKE_CURRENT_LIST_DIR}/linux/find-clang.cmake")
 
 set(target mipsel-linux-gnu)
 
---
+++ linux/find-clang.cmake
@@ -0,0 +1,11 @@
+find_program(
+  clang
+  NAMES clang-21 clang-20 clang-19 clang-18 clang
+  REQUIRED
+)
+
+find_program(
+  clang++
+  NAMES clang++-21 clang++-20 clang++-19 clang++-18 clang++
+  REQUIRED
+)
