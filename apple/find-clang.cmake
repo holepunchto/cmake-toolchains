82fdda4c023055855772b9e80441355329e51734
Kasper Isager Dalsgarð <kasperisager@hey.com>
2025-06-03 10:35:32 +0200

    Set `CMAKE_CXX_COMPILER_CLANG_SCAN_DEPS` on Apple platforms

--- apple/find-clang.cmake
+++ apple/find-clang.cmake
@@ -8,6 +8,7 @@ find_program(
 find_program(
   clang
   NAMES clang
+  REQUIRED
 )
 
 find_program(
@@ -20,4 +21,17 @@ find_program(
 find_program(
   clang
   NAMES clang++
+  REQUIRED
+)
+
+find_program(
+  clang-scan-deps
+  NAMES clang-scan-deps
+  PATHS /opt/homebrew/opt/llvm/bin
+  NO_DEFAULT_PATH
+)
+
+find_program(
+  clang-scan-deps
+  NAMES clang-scan-deps
 )
