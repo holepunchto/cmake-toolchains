c5bebdccede5366b3d477bb1036396959bd61f11
Kasper Isager Dalsgarð <kasperisager@hey.com>
2025-06-19 09:10:41 +0200

    Run OpenMP test

--- test.js
+++ test.js
@@ -2,6 +2,7 @@ const { compile } = require('./test/helpers')
 
 for (const fixture of [
   'test/fixtures/c/executable',
+  'test/fixtures/c/openmp',
   'test/fixtures/c/shared-library',
   'test/fixtures/c/static-library',
   'test/fixtures/cxx/executable',
