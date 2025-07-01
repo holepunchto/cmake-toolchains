2bd651e191ca239fc05aa667f6687c4c4b36ccd5
Kasper Isager Dalsgarð <kasperisager@hey.com>
2025-07-01 16:30:08 +0200

    Use and set `ENV{ANDROID_NDK_HOME}` (#4)

--- android/find-ndk.cmake
+++ android/find-ndk.cmake
@@ -9,20 +9,26 @@ endif()
 
 set(ANDROID_HOME "$ENV{ANDROID_HOME}")
 
-file(GLOB ANDROID_NDK_CANDIDATES RELATIVE "${ANDROID_HOME}/ndk" "${ANDROID_HOME}/ndk/*")
+if(DEFINED ENV{ANDROID_NDK_HOME})
+  set(ANDROID_NDK "$ENV{ANDROID_NDK_HOME}")
+else()
+  file(GLOB ANDROID_NDK_CANDIDATES RELATIVE "${ANDROID_HOME}/ndk" "${ANDROID_HOME}/ndk/*")
 
-list(FILTER ANDROID_NDK_CANDIDATES INCLUDE REGEX "^[0-9]+\.[0-9]+\.[0-9]+$")
+  list(FILTER ANDROID_NDK_CANDIDATES INCLUDE REGEX "^[0-9]+\.[0-9]+\.[0-9]+$")
 
-if(NOT ANDROID_NDK_CANDIDATES)
-  message(
-    FATAL_ERROR
-    "No usable Android NDK was found in ANDROID_HOME. See "
-    "https://developer.android.com/studio/projects/install-ndk"
-  )
+  if(NOT ANDROID_NDK_CANDIDATES)
+    message(
+      FATAL_ERROR
+      "No usable Android NDK was found in ANDROID_HOME. See "
+      "https://developer.android.com/studio/projects/install-ndk"
+    )
+  endif()
+
+  list(SORT ANDROID_NDK_CANDIDATES COMPARE NATURAL ORDER DESCENDING)
+
+  list(POP_FRONT ANDROID_NDK_CANDIDATES ANDROID_NDK_VERSION)
+
+  set(ANDROID_NDK "${ANDROID_HOME}/ndk/${ANDROID_NDK_VERSION}")
+
+  set(ENV{ANDROID_NDK_HOME} "${ANDROID_NDK}")
 endif()
-
-list(SORT ANDROID_NDK_CANDIDATES COMPARE NATURAL ORDER DESCENDING)
-
-list(POP_FRONT ANDROID_NDK_CANDIDATES ANDROID_NDK_VERSION)
-
-set(ANDROID_NDK "${ANDROID_HOME}/ndk/${ANDROID_NDK_VERSION}")
