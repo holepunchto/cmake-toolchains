5079a07ab9660c6db95bb20ab411f4ca4d6219e6
Kasper Isager Dalsgarð <kasperisager@hey.com>
2026-02-25 13:08:23 +0100

    Reenable LLVM 21

--- apple/find-clang.cmake
+++ apple/find-clang.cmake
@@ -1,10 +1,8 @@
-# TODO This intentionally skips LLVM 21 in preference of 20 due to:
-# https://github.com/llvm/llvm-project/issues/155531
-
 find_program(
   clang
   NAMES clang
   PATHS
+    /opt/homebrew/opt/llvm@21/bin
     /opt/homebrew/opt/llvm@20/bin
     /opt/homebrew/opt/llvm@19/bin
     /opt/homebrew/opt/llvm@18/bin
@@ -22,6 +20,7 @@ find_program(
   clang++
   NAMES clang++
   PATHS
+    /opt/homebrew/opt/llvm@21/bin
     /opt/homebrew/opt/llvm@20/bin
     /opt/homebrew/opt/llvm@19/bin
     /opt/homebrew/opt/llvm@18/bin
@@ -39,6 +38,7 @@ find_program(
   clang-scan-deps
   NAMES clang-scan-deps
   PATHS
+    /opt/homebrew/opt/llvm@21/bin
     /opt/homebrew/opt/llvm@20/bin
     /opt/homebrew/opt/llvm@19/bin
     /opt/homebrew/opt/llvm@18/bin
