1651d5790c471dc06da28738cdca86d19d812cd1
Kasper Isager Dalsgarð <kasperisager@hey.com>
2026-02-25 13:14:36 +0100

    Add experimental `mingw-arm64` toolchains
    It hasn't been upstreamed to MinGW yet.

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ exports['linux-mipsel-musl'] = require.asset('./linux-mipsel-musl.cmake', __file
 exports['linux-mipsel-muslsf'] = require.asset('./linux-mipsel-muslsf.cmake', __filename)
 exports['linux-x64'] = require.asset('./linux-x64.cmake', __filename)
 exports['linux-x64-musl'] = require.asset('./linux-x64-musl.cmake', __filename)
+exports['mingw-arm64'] = require.asset('./mingw-arm64.cmake', __filename)
 exports['mingw-x64'] = require.asset('./mingw-x64.cmake', __filename)
 exports['win32-arm64'] = require.asset('./win32-arm64.cmake', __filename)
 exports['win32-x64'] = require.asset('./win32-x64.cmake', __filename)
---
+++ mingw-arm64.cmake
@@ -0,0 +1,35 @@
+set(CMAKE_SYSTEM_NAME Windows)
+set(CMAKE_SYSTEM_PROCESSOR AMD64)
+
+find_program(
+  cc
+  NAMES
+    aarch64-w64-mingw32-gcc
+    aarch64-w64-mingw32-gcc.exe
+  REQUIRED
+)
+
+find_program(
+  c++
+  NAMES
+    aarch64-w64-mingw32-g++
+    aarch64-w64-mingw32-g++.exe
+  REQUIRED
+)
+
+set(target aarch64-w64-mingw32)
+
+set(CMAKE_C_COMPILER ${cc})
+set(CMAKE_C_COMPILER_TARGET ${target})
+
+set(CMAKE_CXX_COMPILER ${c++})
+set(CMAKE_CXX_COMPILER_TARGET ${target})
+
+set(CMAKE_ASM_COMPILER ${cc})
+set(CMAKE_ASM_COMPILER_TARGET ${target})
+
+set(CMAKE_EXE_LINKER_FLAGS_INIT "-static")
+
+set(CMAKE_POSITION_INDEPENDENT_CODE ON)
+
+set(VCPKG_TARGET_TRIPLET arm64-mingw)
